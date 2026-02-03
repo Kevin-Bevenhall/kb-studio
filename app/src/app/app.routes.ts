d71afd041ff0a4aa55800ac14d10e91a9a0ff2e2
Kevin B <k.bevenhall@gmail.com>
2026-02-03 22:04:35 +0100

    sidenav layout

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { SidenavComponent } from './core/components/sidenav/sidenav.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./core/components/sidenav/sidenav.component').then(m => m.SidenavComponent),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
+      }
+    ]
+  }
+];
--- app/src/app/core/components/sidenav/sidenav.component.ts
+++ app/src/app/core/components/sidenav/sidenav.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, viewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatIconModule } from '@angular/material/icon'
+import { MatRippleModule } from '@angular/material/core'
+import { MatDividerModule } from '@angular/material/divider';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  imports: [MatSidenavModule, MatButtonModule, MatListModule, MatIconModule, MatRippleModule, MatDividerModule, RouterOutlet, 
+  RouterLink, RouterLinkActive],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
+  sidenav = viewChild.required(MatSidenav);
+  isCollapsed = signal(false);
+  sidenavWidth = computed(() => this.isCollapsed() ? '56px' : '255px');
 
+  menuItems = [
+    { label: 'Home', path: 'home', icon: 'home' },
+    { label: 'Calendar', path: 'calendar', icon: 'calendar_month' },
+    { label: 'Board', path: 'board', icon: 'developer_board' },
+    { label: 'Movies', path: 'movie', icon: 'videocam' },
+  ];
+
+  toggleSidenav() {
+    this.isCollapsed.set(!this.isCollapsed());
+  }
 }
