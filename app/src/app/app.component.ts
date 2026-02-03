d112fa637244bb6e5f16f0050f192663098db9ca
Kevin B <k.bevenhall@gmail.com>
2026-02-03 18:07:28 +0100

    core, features, shared scaffolding

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class App {
+}
