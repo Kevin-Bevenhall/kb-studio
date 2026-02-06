76b394ec78cc46ab799463c90fe89b9eb1061399
Kevin B <k.bevenhall@gmail.com>
2026-02-06 21:55:33 +0100

    Global: navigation progress bar (will change in future to something else)

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Router, RouterOutlet } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './shared/services/auth.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, MatProgressBarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,12 +15,26 @@ export class App {
   private authService = inject(AuthService);
   private router = inject(Router);
 
+  loading = signal(false);
+
   constructor() {
     this.authService.user$.pipe(
       takeUntilDestroyed()
     ).subscribe((user) => {
       if (!user) {
         this.router.navigate(['']);
+      }
+    });
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading.set(true);
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError 
+      ) {
+        this.loading.set(false);
       }
     })
   }
