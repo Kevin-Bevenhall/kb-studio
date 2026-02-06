8e04470173aeccc6d421014ff5aa60b6e0250901
Kevin B <k.bevenhall@gmail.com>
2026-02-06 18:23:25 +0100

    Movie: Detail styling

--- app/src/app/features/movie/components/movie-detail/movie-detail.component.ts
+++ app/src/app/features/movie/components/movie-detail/movie-detail.component.ts
@@ -1,10 +1,14 @@
 import { Component, inject, input } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
 import { ActivatedRoute, Router } from '@angular/router';
 import { MovieDetail } from '../../../../shared/models/movie/movie-detail';
+import { MatCardModule } from '@angular/material/card'
+import { MatChipsModule } from '@angular/material/chips'
 
 @Component({
   selector: 'app-movie-detail',
-  imports: [],
+  imports: [MatButtonModule, MatIcon, MatCardModule, MatChipsModule],
   templateUrl: './movie-detail.component.html',
   styleUrl: './movie-detail.component.scss',
 })
@@ -15,6 +19,6 @@ export class MovieDetailComponent {
   movie = input.required<MovieDetail>();
 
   close() {
-    this.router.navigate(['../'], { relativeTo: this.route, skipLocationChange: false})
+    this.router.navigate(['../'], { relativeTo: this.route, skipLocationChange: false });
   }
 }
